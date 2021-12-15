7495ca54fd03cb28b887b2f4c552a2fd7d96f107
calaisgalbraith <calaisgalbraith@gmail.com>
2021-12-14 21:32:39 -0600

    first commit

---
+++ Codecademy-NET-C-Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codecademy_NET_C_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
